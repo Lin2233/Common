1678b498e89f86d9c3122ac4726b702e5f196355
yulp <yulp@visionet.com.cn>
2017-03-02 14:22:43 +0800

    no message

---
+++ AnimationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationTest
+//
+//  Created by YuLinpo on 2016/11/23.
+//  Copyright © 2016年 Yulinpo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimationTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnimationTest
+//
+//  Created by YuLinpo on 2016/11/23.
+//  Copyright © 2016年 Yulinpo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
